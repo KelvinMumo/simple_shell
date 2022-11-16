fdb9249554f7eb91a532629bbba2496a0db0de31
KelvinMumo <kelvinmumo977@gmail.com>
2022-11-16 15:27:37 +0000

    getline

---
+++ _getline.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints "$ "
+ * wait for the user to enter a command, prints it on the next line
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char *buffer = malloc(1024);
+	size_t len = 1024;
+
+	while (1)
+	{
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		printf("%s", buffer);
+	}
+	return (0);
+}
